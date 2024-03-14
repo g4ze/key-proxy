558fcf2e578bfead15ef63467883640e76e4a774
g4ze <bhaiyajionline17@gmail.com>
2024-03-14 16:08:45 +0530

    add hotkeys

--- src/key_proxy_gui.py
+++ src/key_proxy_gui.py
@@ -8,19 +8,20 @@ from kivy.core.window import Window
 from kivy.clock import Clock
 from kivy.uix.modalview import ModalView
 
-
 class MyBoxLayout(BoxLayout):
+    stop_key = "ctrl+alt+x"
+    start_key = "ctrl+alt+s"
     def __init__(self, **kwargs):
         super(MyBoxLayout, self).__init__(**kwargs)
         self.orientation = "horizontal"
+        self.size = (800, 600)  # Set a fixed size for the layout
 
         # Left box for text input
-        left_box = BoxLayout(orientation="vertical")
+        left_box = BoxLayout(orientation="vertical", size_hint_x=0.5, size=(400, 600))
 
-       
         self.text_input = TextInput(
             hint_text="Paste your text here:",
-            size_hint=(1, 0.8),
+            size_hint_y=0.8,
             background_color=(0, 0, 0, 1),
             foreground_color=(1, 1, 1, 1),
         )
@@ -29,35 +30,65 @@ class MyBoxLayout(BoxLayout):
         self.add_widget(left_box)
 
         # Right box for buttons
-        right_box = BoxLayout(orientation="vertical")
-         # Widget for support link
+        right_box = BoxLayout(orientation="vertical", size_hint_x=0.5, size=(400, 600))
+
+        # Widget for support link
         self.support_widget = Button(
             text="WELCOME! Reach developer by clicking here!",
-             size_hint=(1, 0.1), size_hint_max_x=400, size_hint_max_y=100,
+            size_hint_y=None, height=100,
             background_color=(0.5, 0.5, 0.5, 1),
             color=(1, 1, 1, 1),
         )
         self.support_widget.bind(on_press=self.open_support_link)
         right_box.add_widget(self.support_widget)
 
-
+        # Duration input box
+        duration_input_box = BoxLayout(orientation="horizontal", size_hint_y=None, height=50)
+        duration_label = Label(text="duration:", color=(1, 1, 1, 1), size_hint_x=None, width=150)
         self.countdown_input = TextInput(
             hint_text="Enter countdown duration (in seconds):",
-            size_hint=(1, 0.1),
+            text="5",
             background_color=(0, 0, 0, 1),
             foreground_color=(1, 1, 1, 1),
         )
-        right_box.add_widget(self.countdown_input)
+        duration_input_box.add_widget(duration_label)
+        duration_input_box.add_widget(self.countdown_input)
+        right_box.add_widget(duration_input_box)
+
+        # Hot-key-start input box
+        hotkey_input_box = BoxLayout(orientation="horizontal", size_hint_y=None, height=50)
+        hotkey_label = Label(text="hot-key to start:", color=(1, 1, 1, 1), size_hint_x=None, width=150)
+        self.hotkey_input = TextInput(
+            hint_text="Enter hot-key to stop countdown",
+            text="ctrl+alt+s",
+            background_color=(0, 0, 0, 1),
+            foreground_color=(1, 1, 1, 1),
+        )
+        hotkey_input_box.add_widget(hotkey_label)
+        hotkey_input_box.add_widget(self.hotkey_input)
+        right_box.add_widget(hotkey_input_box)
+
+        # Hot-key-stop input box
+        hotkey_input_box = BoxLayout(orientation="horizontal", size_hint_y=None, height=50)
+        hotkey_label = Label(text="hot-key to stop:", color=(1, 1, 1, 1), size_hint_x=None, width=150)
+        self.hotkey_input = TextInput(
+            hint_text="Enter hot-key to stop countdown",
+            text="ctrl+alt+x",
+            background_color=(0, 0, 0, 1),
+            foreground_color=(1, 1, 1, 1),
+        )
+        hotkey_input_box.add_widget(hotkey_label)
+        hotkey_input_box.add_widget(self.hotkey_input)
+        right_box.add_widget(hotkey_input_box)
 
         self.button_execute = Button(
-            text="Start", size_hint=(1, 0.1), size_hint_max_x=400, size_hint_max_y=100,
+            text="Start", size_hint_y=None, height=100,
             background_color=(0, 2, 0, 2)
         )
         self.button_execute.bind(on_press=self.start_execution)
         right_box.add_widget(self.button_execute)
 
         self.add_widget(right_box)
-
         
 
 
@@ -130,7 +161,7 @@ class MyBoxLayout(BoxLayout):
 
 class MyApp(App):
     def build(self):
-        Window.size = (400, 300)
+        Window.size = (700, 400)
         self.title = "Key-Proxy"
         return MyBoxLayout()
 
